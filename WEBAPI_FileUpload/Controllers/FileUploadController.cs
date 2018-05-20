22222d4c19b62d77f83f3afa7e4a3b71560bf64b
raviranjankr <im.rranjank@hotmail.com>
2018-05-20 20:36:46 +0530

    Add project files.

---
+++ WEBAPI_FileUpload/Controllers/FileUploadController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using WEBAPI_FileUpload.UploadRepo;
+
+namespace WEBAPI_FileUpload.Controllers
+{
+
+    public class FileUploadController : ApiController
+    {
+        [Mime]
+        public async Task<FileUploadDetails> Post()
+        {
+            // file path
+            var fileuploadPath = HttpContext.Current.Server.MapPath("~/UploadedFiles");
+
+            // 
+            var multiFormDataStreamProvider = new MultiFileUploadProvider(fileuploadPath);
+
+            // Read the MIME multipart asynchronously 
+            await Request.Content.ReadAsMultipartAsync(multiFormDataStreamProvider);
+
+            string uploadingFileName = multiFormDataStreamProvider
+                .FileData.Select(x => x.LocalFileName).FirstOrDefault();
+
+            // Create response
+            return new FileUploadDetails
+            {
+
+                FilePath = uploadingFileName,
+
+                FileName = Path.GetFileName(uploadingFileName),
+
+                FileLength = new FileInfo(uploadingFileName).Length,
+
+                FileCreatedTime = DateTime.Now.ToLongDateString()
+            };
+        }
+    }
+}
---
+++ WEBAPI_FileUpload/UploadRepo/FileUploadDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+
+namespace WEBAPI_FileUpload.UploadRepo
+{
+    public class FileUploadDetails 
+    {
+        public string FilePath { get; set; }
+        public string FileName { get; set; }
+        public long FileLength { get; set; }
+        public string FileCreatedTime { get; set; }
+        
+    }
+}
